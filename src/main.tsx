80a8dd37c8ea8ec6beeca6cd6c63f16383f19e87
Lachlan Nuttall <github@maker.com.au>
2025-03-05 18:04:07 +1000

    Enabling strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-const isStrict = false;
+const isStrict = true;
 createRoot(document.getElementById('root')!).render(
   isStrict ? (
     <StrictMode>
